ac55facef62bcefcf659110c7bd3bf6fa33a2281
Jan S. Kostlán <jan.kostlan.021@pslib.cz>
2024-03-26 19:03:52 +0100

    nevím jsem z toho špatný

--- Anti-Monopoly/src/App.tsx
+++ Anti-Monopoly/src/App.tsx
@@ -11,6 +11,7 @@ import MainMenu from './components/MainMenu/MainMenu.tsx';
 import ChoiceMenu from './components/ChoiceMenu/ChoiceMenu.tsx';
 import Rules from './components/Rules/Rules.tsx';
 import ErrorPage from './error-page.tsx';
+import Playing from './components/Playing/Playing.tsx';
 
 // Vytvoření routeru s nevnořenými cestami
 const router = createBrowserRouter(
@@ -18,7 +19,8 @@ const router = createBrowserRouter(
     <Route path="/" element={<MainMenu />} />,
     <Route path="choicemenu" element={<ChoiceMenu />} />,
     <Route path="rules" element={<Rules />} />,
-    <Route path="*" element={<ErrorPage />} />]  )
+    <Route path="*" element={<ErrorPage />} /> ,
+    <Route path="Playing" element={<Playing />} />])
 );
 
 function App() {
--- Anti-Monopoly/src/components/Playing/Board.tsx
+++ Anti-Monopoly/src/components/Playing/Board.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import styles from './Rules.module.css';
-import { Link } from "react-router-dom";
+import styles from './Board.module.css';
+
 
 const Board = () => {
+    const fields: string[] = Array(169).fill(null);
 
     return (
         <>
-        <div className="board">
-
+        <div className={styles["board"]}>
+            {fields.map((_, i) => {
+                const row = Math.floor(i / 13);
+                const col = i % 10;
+                let className = styles["field"];
+                if (row === 0 && col === 0) { // První řádek a první sloupec
+                    className = `${styles["field"]} ${styles["double-height"]} ${styles["double-width"]}`;
+                } else if (row === 12 && col === 12) { // Poslední řádek a poslední sloupec
+                    className = `${styles["field"]} ${styles["double-height"]} ${styles["double-width"]}`;
+                } else if (row === 0 || col === 0) { // První řádek nebo první sloupec
+                    className = `${styles["field"]} ${styles["double-height"]}`;
+                } else if (row === 12 || col === 12) { // Poslední řádek nebo poslední sloupec
+                    className = `${styles["field"]} ${styles["double-width"]}`;
+                }
+                return <div className={className} key={i}></div>;
+            })}
         </div>
         </>
     );
